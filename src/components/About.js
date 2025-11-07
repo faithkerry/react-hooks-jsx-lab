f9e007dfd2553001b8f6490f7e3b99b3feb5ebf7
faith-kerry <kerubofaith944@gmail.com>
2025-11-07 22:22:21 +0300

    Completed the reach hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm passionate about building interactive and creative web apps!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
